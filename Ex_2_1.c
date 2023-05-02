1963426dbd86b354dc4b7a387e2660575fb4334a
Gerson Santos <gersones@outlook.com>
2023-05-02 13:52:52 -0300

    aa

--- Ex_2_1.c
+++ Ex_2_1.c
@@ -1,8 +1,8 @@
+
 #include <stdio.h>
 
 int main(){
 	printf("Oi, tudo bem? Tenho 6 anos e programo.\n");
-	
 	printf("Valor inteiro: %d.\n", 10);
 	printf("Valor real: %f.\n", 3.14159265);
 	printf("Valor real com apenas duas casas: %.2f.\n", 3.14159265);
