9c8f8975ea2f24eb47a4ab6c02580e6bbd073ccd
Gerson Santos <gersones@outlook.com>
2023-06-04 14:38:25 -0300

    a

--- Ex_1_1.c
+++ Ex_1_1.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+
+
 int main(){
-	printf("Hello, world!\n");
-	system("pause");
+    float imc = 0.0;
+    float mult = 0.0;
+    int idade = 0;
+    float altura = 0.0;
+    int peso = 0;
+    char nome[50] = "";
+   
+    printf("Digite a idade:\n");
+    scanf("%d", &idade);
+   
+    printf("Digite a altura:\n");
+    scanf("%f", &altura);
+
+    printf("Digite o peso:\n");
+    scanf("%d", &peso);
+
+    printf("Digite o nome:\n");
+    scanf("%s", nome);
+       
+    printf("Dados informados:\n");
+    printf("Idade: %d.\n", idade);
+    printf("Altura: %.2f.\n", altura);
+    printf("peso: %.d.\n", peso);
+    printf("Nome: %s.\n", nome);
+    mult = altura * altura;
+    imc =  peso / mult;
+   
+    printf("IMC: %.2f.\n", imc);
+
 }
